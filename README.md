115294d7d3188d34629bc464cc6a1306e64e768a
Tomáš Suchanek <tomassuchanek96@gmail.com>
2025-03-16 15:22:38 +0100

    Initial commit - basic structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# shopping_list_suchanek_erasmus
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/shopping_list_suchanek_erasmus/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shopping_list_suchanek_erasmus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
